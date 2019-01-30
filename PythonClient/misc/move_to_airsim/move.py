5f0c0aaa26d8bba52ac5a6b23b92eba20d85aae9
Srivatsan Krishnan <91.srivatsan@gmail.com>
2019-01-30 11:29:00 -0600

    Adding copy utility to AirSim PythonClient

---
+++ PythonClient/misc/move_to_airsim/move.py
@@ -0,0 +1,10 @@
+import airsim
+import os
+
+from shutil import copy2
+airsim_dir = os.path.dirname(airsim.__file__)
+file_path = os.path.realpath(__file__)
+dir_path = os.path.dirname(file_path)
+copy2(dir_path+"\\client.py", airsim_dir)
+copy2(dir_path+"\\types.py", airsim_dir)
+
