a6495c9bb2edc7bb276b63082956851c5cf86c3a
wfu <wfu@college.harvard.edu>
2019-03-18 13:16:14 -0400

    made environment tick based, cleaned a lot of stuff up

--- CrazyFlieObstacleAvoidance/environment.py
+++ CrazyFlieObstacleAvoidance/environment.py
@@ -16,40 +16,102 @@ class ObstacleEnvironment(object):
     and have them move toward the crazyflie."""
     def __init__(self, client):
         self.obstacles = ['Obstacle1', 'Obstacle2'] 
+        self.obstacle_pos = dict()
+        self.original_pos = dict()
         self.c = client
+        self.moving_objects = 0
+        self.resetting = False
 
+        # Get the original positions of all the obstacles to reset it later
+        for obj_id in self.obstacles:
+            self.original_pos[obj_id] = self.c.simGetObjectPose(obj_id)
+
+    def reset(self):
+        """Reset all objects to original state"""
+        for obj_id, pose in self.original_pos.items():
+            self.c.simSetObjectPose(obj_id, pose, teleport=True)
+        
+        self.resetting = True
+        while self.moving_objects > 0:
+            print("Polling until all environment threads done to reset.")
+            pass
+        self.resetting = False
+        self.obstacle_pos = dict()
+        
 
     def _move_obstacle_thread(self, obj_id, pos_start, pos_end, dur):
         """Takes a tuple of object_id, start_pose, end_pose, and duration and
-        turns this into smooth movement manually."""
+        turns this into smooth movement manually. Unfortunately, airsim is
+        very not thread safe, so we'll actually just use this thread to update
+        where our object SHOULD be and then have a tick function that updates"""
         pos_curr = copy.deepcopy(pos_start)
         time_start = time.time()
-        print("Trying to move %s" % obj_id)
+        self.moving_objects += 1
         while time.time() - time_start < dur:
             time_diff = time.time() - time_start
             diff = (pos_end.position - pos_start.position) * time_diff / float(dur)
             pos_curr.position = diff + pos_start.position
-            print("Moving to position %s" % str(pos_curr.position))
-            succ = self.c.simSetObjectPose(obj_id, pos_curr, teleport=True)
-            time.sleep(0.1)
-        return
-
+            # Store where the obstacle should be in a dictionary.
+            self.obstacle_pos[obj_id] = pos_curr
+            if self.resetting:
+                break
+        self.moving_objects -= 1
+    
+    def tick(self):
+        """Actually moves the obstacles to where they should be. We need this
+        in our main loop since airsim will die if you run the obstacle movement
+        on its own thread due to tornado"""
+        for obstacle_id, pose in self.obstacle_pos.items():
+            self.c.simSetObjectPose(obstacle_id, pose, teleport=False)
 
     def move_obstacle(self):
-        """Gets a random obstacle and moves it for a few seconds"""
+        """Gets a random obstacle and moves it for a few seconds. Since we are
+        moving with collisions turned on, make SURE that your the obstacle
+        you are moving isn't touching another obstacle."""
         obstacle_id = np.random.choice(self.obstacles)
         pose1 = self.c.simGetObjectPose(obstacle_id)
         pose2 = copy.deepcopy(pose1)
-        pose2.position = pose1.position + airsim.Vector3r(20, 20, 20)
+        dx, dy = np.random.uniform(-20, 20), np.random.uniform(-20, 20)
+        pose2.position = pose1.position + airsim.Vector3r(dx, dy, 0)
         thread = threading.Thread(target=self._move_obstacle_thread,
                                   args=(obstacle_id, pose1, pose2, 5.0))
         thread.start()
+    
+    def move_obstacle_at_drone(self, speed=1.0):
+        pos_drone = self.c.getMultirotorState().kinematics_estimated.position
+        x, y = pos_drone.x_val, pos_drone.y_val
+        print("Drone currently at coords %s" % str((x, y)))
+        obstacle_id = np.random.choice(self.obstacles)
+        pose1 = self.c.simGetObjectPose(obstacle_id)
+        pose2 = copy.deepcopy(pose1)
+        pose2.position = pose1.position + (airsim.Vector3r(x, y, 0) - pose1.position) * 2
+        dist = pose1.position.distance_to(pose2.position)
+        thread = threading.Thread(target=self._move_obstacle_thread,
+                                  args=(obstacle_id, pose1, pose2, dist / speed))
+        thread.start()
+    
+    def _distance_from_object(self, x, y, object_id):
+        """Finds distance away from specific object id.
+            @param x (float): x location of the drone
+            @param y (float): y location of the drone
+            @param object_id (str): obstacle_id, a string
+        """
+        pose1 = self.c.simGetObjectPose(object_id)
+        obj_x, obj_y = pose1.position.x_val, pose1.position.y_val
+        return ((obj_x - x) ** 2 + (obj_y - y) ** 2) ** 0.5
         
+    def distance_from_nearest_object(self):
+        """Calculates distance of drone from nearest object"""
+        pos_drone = self.c.getMultirotorState().kinematics_estimated.position
+        x, y = pos_drone.x_val, pos_drone.y_val
+        return min([self._distance_from_object(x, y, obj_id) for obj_id in self.obstacles])
+
 
 if __name__ == '__main__':
     c = airsim.MultirotorClient()
     c.confirmConnection()
     c.enableApiControl(True)
+    c.takeoffAsync().join()
 
     a = ObstacleEnvironment(c)
-    a.move_obstacle()
+    a.move_obstacle_at_drone()
--- CrazyFlieObstacleAvoidance/simple_fc.py
+++ CrazyFlieObstacleAvoidance/simple_fc.py
@@ -1,15 +1,18 @@
-import airsim
+import time
+
 import numpy as np
+import airsim
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import torchvision.transforms as T
-from models_torch import SimpleFCDQN
-import time
+
+from models_torch import SimpleFCDQN, ReplayMemory
+from environment import ObstacleEnvironment
+
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
 
 def action2motion(action, speed=0.25):
     """Turns an action from our fully connected net and turns it into movement.
@@ -27,9 +30,19 @@ def action2motion(action, speed=0.25):
     except KeyError:
         raise RuntimeError("Could not convert discrete action into movement.")
 
-def compute_reward(quad_state, quad_vel, collision_info):
-    if collision_info.has_collided:
-        reward = -100
+def compute_reward(env, collision_info):
+    d = env.distance_from_nearest_object()
+    # linearly interpolate negative reward for having the obstacle too close
+    reward = 0
+    CRASH_DISTANCE = 0.75
+    CLOSE_DISTANCE = 1.5
+    NEAR_DISTANCE = 5 
+    if d < CRASH_DISTANCE:
+        reward += -200
+    elif d < CLOSE_DISTANCE:
+        reward += -100
+    elif d < NEAR_DISTANCE:
+        reward += -100 + 100 * (d - CLOSE_DISTANCE) / (NEAR_DISTANCE - CLOSE_DISTANCE)
     
     return reward
 
@@ -39,20 +52,45 @@ if __name__ == '__main__':
     a.confirmConnection()
     a.enableApiControl(True)
     a.armDisarm(True)
+    env = ObstacleEnvironment(a)
     
     for _ in range(2):
         a.reset()
+        env.reset()
         a.confirmConnection()
         a.enableApiControl(True)
         a.armDisarm(True)
-        a.takeoffAsync().join()
-        for _ in range(100):
-            a.moveByVelocityAsync(2.0, 0, 0, 5)
-            time.sleep(0.5)
-            a.moveByVelocityAsync(-2.0, 0, 0, 5)
-            time.sleep(0.5)
-
-        collision_info = client.simGetCollisionInfo()
-        if collision_info.has_collided:
-            print("WE HIT SOMETHING!@@@")
+        
+        COMMAND_DELAY = 0.2
+        last_command = time.time()
+        reward = 0
+        for _ in range(10000):
+            # Only perform a command after a delay, simulating the amount of time
+            # it takes the drone to actually run control and send commands to the
+            # motors. Also moving back and forth causes the drone to go vertical
+            # a little bit so make sure it hovers at the same height.
+            collision_info = a.simGetCollisionInfo()
+            if time.time() - last_command > COMMAND_DELAY:
+                last_command = time.time()
+                x, y = np.random.uniform(-2, 2), np.random.uniform(-2, 2) 
+                height = a.getMultirotorState().kinematics_estimated.position.z_val
+                STABLE_HEIGHT = -0.5
+                z_comp = np.clip(STABLE_HEIGHT - height, -0.5, 0.5)
+                print(height)
+                a.moveByVelocityAsync(x, y, z_comp, 0.1)
+                reward = compute_reward(env, collision_info)
+                print("Current reward: %f" % reward)
+        
+            if env.moving_objects == 0:
+                env.move_obstacle_at_drone()
+            
+            env.tick()
+            
+            if reward < -150 or collision_info.has_collided:
+                # Reset the environment, we've gotten BOOMED
+                break
+            
+            
+    env.reset()
+    a.reset()
 
